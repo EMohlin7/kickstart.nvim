706e7cd9559d622e5109e87a8c7d29fbc5685e5f
Edvin Mohlin <edvin.mohlin@external.epiroc.com>
2026-04-14 17:05:58 +0200

    nerd font

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -628,7 +628,7 @@ require('lazy').setup({
           '--function-arg-placeholders=false',
           '--header-insertion=never', -- Insert what you use. Insert header on autocomplete
           '--header-insertion-decorators', -- Show a circle in the autocomplete list when a header will be inserted
-          '-j=16', -- Number of CPUs to use
+          '-j=32', -- Number of CPUs to use
         },
         -- gopls = {},
         -- pyright = {},
